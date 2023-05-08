8a7ecb54e22a98162d9d0d4468d8efffaf8e9ed3
mgarcia.devinuesa <mgarcia.devinuesa@atsistemas.com>
2023-05-08 12:36:47 +0200

    Add backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  
+  backend "azurerm" {
+    resource_group_name  = "<RESOURCE_GROUP_NAME>"
+    storage_account_name = "<STORAGE_ACCOUNT_NAME>"
+    container_name       = "<CONTAINER_NAME>"
+    key                  = "sops.terraform.tfstate"
+  }
+
   required_providers {
     sops = {
       source = "carlpett/sops"
